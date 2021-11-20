47b929a12f7604711e7ebe38fb0ed999e45a2faf
CoCoolSun <819870517@qq.com>
2021-11-20 22:03:38 +0800

    cors htlmet ratelimiconfig 配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from "@nestjs/platform-express";
+
+// HTTP头安全
+import * as helmet from 'helmet';
+// 限速及频率
+import * as rateLimit from 'express-rate-limit';
+import rateLimitConfig from './config/express-rate-limit-config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
+  app.set('trust proxy', 1);
+  app.enableCors();
+  app.use(helmet());
+  app.use('/', rateLimit(rateLimitConfig))
+  await app.listen(8006);
 }
 bootstrap();
