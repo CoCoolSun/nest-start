f6750dd5324ada3272019f42f4cdb21d3df89e10
CoCoolSun <819870517@qq.com>
2021-11-24 17:14:32 +0800

    上传下载

--- src/modules/upload-download/upload-download.service.ts
+++ src/modules/upload-download/upload-download.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
-
+import { zip } from 'compressing';
+import { ConfigService } from 'nestjs-config';
 @Injectable()
-export class UploadDownloadService {}
+export class UploadDownloadService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async downloadAll() {
+    const uploadDir = this.configService.get('multerModuleConfig').root;
+    const tarStream = new zip.Stream();
+    await tarStream.addEntry(uploadDir);
+    return { filename: `${new Date().getTime()}.zip`, tarStream };
+
+  }
+}
